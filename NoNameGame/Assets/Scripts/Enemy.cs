487ff4525f006caa5fa5db7f918955bdebf9c95f
parallelXYZ <lnone67@gmail.com>
2025-09-11 14:42:59 -0600

    enemy spawn code

---
+++ NoNameGame/Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : AnimatedEntity
+{
+    public static int maxEnemies = 5;
+    public static int enemyCount = 0;
+    public int hp = 1;
+    private IEnumerator spawnRoutine;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AnimationSetup();
+        spawnRoutine = Spawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemyCount < maxEnemies) {
+            StartCoroutine(spawnRoutine);
+        } else {
+            StopCoroutine(spawnRoutine);
+        }
+        if (hp <= 0) {
+            Destroy(this);
+        }
+    }
+
+    public IEnumerator Spawn()
+    {
+        Instantiate(new Enemy(), this.transform.position, Quaternion.identity); 
+        yield return new WaitForSeconds(2);
+    }
+}

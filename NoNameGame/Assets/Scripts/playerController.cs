680ece299505f143b3606a9c1fe1cbc4cb399d4a
scarstenCS <scarsten@ualberta.ca>
2025-09-11 14:13:27 -0600

    add player script

---
+++ NoNameGame/Assets/Scripts/playerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public int health = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}

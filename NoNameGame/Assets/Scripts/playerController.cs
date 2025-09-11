4c4939b148808b6967de988295b21350d9fac79b
scarstenCS <scarsten@ualberta.ca>
2025-09-11 16:37:28 -0600

    added some wip not working code

--- NoNameGame/Assets/Scripts/playerController.cs
+++ NoNameGame/Assets/Scripts/playerController.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
+    private Transform t;
     public int health = 20;
+    public float startSpeed = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        t = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    
+    private void OnMove(InputValue movementValue)
+    {
+        t.position += new Vector2(movementValue.x, movementValue.y);
+    }
 }

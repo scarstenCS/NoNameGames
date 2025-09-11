d5def42565e1d01ba89570c40c13dc9f0edfbbbe
scarstenCS <scarsten@ualberta.ca>
2025-09-11 17:33:44 -0600

    movement is sorta working???

--- NoNameGame/Assets/Scripts/playerController.cs
+++ NoNameGame/Assets/Scripts/playerController.cs
@@ -1,12 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class playerController : MonoBehaviour
 {
     private Transform t;
     public int health = 20;
     public float startSpeed = 1;
+
+    public PlayerControls controls;
+
+    private InputAction move; 
+
+    private void Awake() {
+        controls = new PlayerControls();
+    }
+    private void OnEnable()
+    {
+        move = controls.Player.Move;
+        move.Enable();
+    }
+
+    private void OnDisable()
+    {
+        move.Disable();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -17,10 +36,10 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        t.position += (Vector3) move.ReadValue<Vector2>();
     }
-    private void OnMove(InputValue movementValue)
-    {
-        t.position += new Vector2(movementValue.x, movementValue.y);
-    }
+    // private void OnMove(Input movementValue)
+    // {
+    //     t.position += new Vector2(movementValue.x, movementValue.y);
+    // }
 }

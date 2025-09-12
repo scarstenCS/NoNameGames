94fa3a1b305fca73ebe90113d832c2c37649db7b
scarstenCS <scarsten@ualberta.ca>
2025-09-12 14:08:56 -0600

    Create basicAttack.cs

---
+++ NoNameGame/Assets/Scripts/basicAttack.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+// https://www.youtube.com/watch?v=LNLVOjbrQj4&t=998s
+public class basicAttack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Vector2 mousePosition;
+
+    public GameObject projectile;
+
+    public float projectileSpeed = 5,
+    projectileMaxDistance =5;
+    private float projectileDistance;
+    private Rigidbody2D rb2d;
+    private Transform t, projectileT;
+
+    private int atkStage = 0;
+    void Start()
+    {
+        t = GetComponent<Transform>();
+        rb2d = GetComponent<Rigidbody2D>();
+        projectileT = GetComponent<Transform>();
+        projectileDistance = projectileMaxDistance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 lookDir;
+        mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()); // from https://www.reddit.com/r/Unity2D/comments/swum6c/how_to_make_object_follow_mouse_in_unity_with_new/
+        lookDir = mousePosition - (Vector2)t.position;
+        rb2d.rotation = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+        Debug.Log(mousePosition);
+
+        PreformAttack();
+
+
+    }
+
+    /// <summary>
+    /// Attempt to start an attack
+    /// </summary>
+    private void Attack()
+    {
+        if (atkStage != 0) return;
+
+        atkStage++;
+    }
+
+    private void PreformAttack()
+    {
+        switch (atkStage)
+        {
+            case 0:
+                return;
+            case 1:
+                projectileT.position += new Vector3(projectileSpeed * Time.deltaTime,0f,0f);
+                if (projectileT.position.x >= projectileDistance)
+                {
+                    atkStage++;
+                }
+                break;
+            case 2:
+                projectileT.position -= new Vector3(projectileSpeed * Time.deltaTime,0f,0f);
+                if (projectileT.position.x >= 0.5)
+                {
+                    projectileT.position = new Vector3(0.5f, 0f, 0f);
+                    atkStage = 0;
+                }
+                break;
+        }
+    }
+    
+}

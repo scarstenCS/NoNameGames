0848a9f21f073f60455346a20d3c3ae01574cc22
Seanpiatt <112649852+Seanpiatt@users.noreply.github.com>
2025-09-13 21:49:04 -0600

    Added UI, Text mesh pro imports, and a camera following the player.
    Added HUD with TextMeshPro: “Health” and “Enemies left”.
    Imported a lot of TextMeshPro files to help with other UI elements and other TMP text endeavors in future.
    Changed Event system to use InputSystemUI module ( I had to change it so let me know if this hurts anyone else).

---
+++ NoNameGame/Assets/Scripts/CameraFollowScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Vector3 myPos;
+    public Transform myPlay;
+
+    void Start()
+    {
+        myPlay = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myPos = transform.position;
+        myPos.x = myPlay.position.x;
+        myPos.y = myPlay.position.y;
+        transform.position = myPos;
+    }
+
+}
---
+++ NoNameGame/Assets/Scripts/UserInterface.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class UserInterface : MonoBehaviour
+{
+    [Header("Wire these in the Inspector")]
+    public Player player;
+    public TMP_Text healthText;              // drag your "Health: ..." TMP Text
+    public TMP_Text enemiesLeftText;         // drag your "Enemies left: ..." TMP Text
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        if (player != null)
+            player.HealthChanged += OnHealthChanged;
+    }
+
+    void OnDisable()
+    {
+        if (player != null)
+            player.HealthChanged -= OnHealthChanged;
+    }
+
+    void Start()
+    {
+        Debug.Log($"[DEBUG] Initializing UI");
+        if (player != null) OnHealthChanged(player.Health, player.maxHealth);
+        Debug.Log($"[DEBUG] Player health: {player.Health} / {player.maxHealth}"); // Debug line
+        if (enemiesLeftText != null) enemiesLeftText.text = "Enemies left: 0";
+
+    }
+
+    private void OnHealthChanged(int current, int max)
+    {
+        
+        if (!healthText) return;
+        Debug.Log($"[DEBUG] Health changed: {current} / {max}"); // Debug line
+
+        healthText.text = $"Health: {current} / {max}";
+    }
+
+        public void SetEnemiesLeft(int count)
+    {
+        if (!enemiesLeftText) return;
+        //TODO: When enemies enter, fix this function
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

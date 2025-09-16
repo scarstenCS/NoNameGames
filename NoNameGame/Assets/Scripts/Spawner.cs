1369777fd591abc0928d01873b4d9539ded60fd2
parallelXYZ <lnone67@gmail.com>
2025-09-15 21:44:48 -0600

    spawn coroutine works

--- NoNameGame/Assets/Scripts/Spawner.cs
+++ NoNameGame/Assets/Scripts/Spawner.cs
@@ -10,30 +10,33 @@ public class Spawner : MonoBehaviour
     public static int maxEnemies = 5;
     public static int enemyCount = 0;
     public bool started = false;
+    private IEnumerator wait;
     public void Start()
     {
         mainCamera = Camera.main;
+        wait = new WaitForSecondsRealtime(2f);
         StartCoroutine(Spawn());
+
     }
 
     public void Update()
     {
-        
+        // if (!started)
+        // {
+        //     StartCoroutine(Spawn());
+        //     started = true;
+        // }
     }
 
     public IEnumerator Spawn()
     {
-        if (enemyCount < maxEnemies)
+        while (enemyCount < maxEnemies)
         {
             Vector3[] positions = { new Vector3(Random.Range(0, 2), Random.Range(0f, 1f)), new Vector3(Random.Range(0f, 1f), Random.Range(0, 2)) };
-           GameObject e  = Instantiate(enemyPrefab, mainCamera.ViewportToWorldPoint(positions[Random.Range(0, 2)]), Quaternion.identity);
+            GameObject e = Instantiate(enemyPrefab, mainCamera.ViewportToWorldPoint(positions[Random.Range(0, 2)]), Quaternion.identity);
             enemyCount++;
             e.GetComponent<Enemy>().player = player;
-            yield return new WaitForSeconds(2f);
-        }
-        else
-        {
-            yield return null;
+            yield return wait;
         }
         
     }

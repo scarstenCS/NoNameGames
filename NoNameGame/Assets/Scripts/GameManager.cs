6b582924b858fa0914860e55ec26d3a6df12d94a
scarstenCS <scarsten@ualberta.ca>
2025-09-22 13:55:00 -0600

    create game manager script

---
+++ NoNameGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    static private GameManager _instance;
+    static public GameManager Instance;
+    void Awake()
+    {
+        _instance = this;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

e12e8d41908965214c1096300ddc76f990991f12
scarstenCS <scarsten@ualberta.ca>
2025-09-24 11:54:48 -0600

    add player take damage method

--- NoNameGame/Assets/Scripts/GameManager.cs
+++ NoNameGame/Assets/Scripts/GameManager.cs
@@ -4,21 +4,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
     static private GameManager _instance;
+
+    static public GameObject player;
     static public GameManager Instance;
     void Awake()
     {
         _instance = this;
     }
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    static public void PlayerTakeDamage(int ammout)
     {
-        
+        Player p = player.GetComponent<Player>();
+        p.TakeDamage(ammout);
     }
+    
 }

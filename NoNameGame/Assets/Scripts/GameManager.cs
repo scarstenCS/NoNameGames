f9473790e3cc4f2724314c45e4864168979e1871
Dragon614pc <115052254+Dragon614pc@users.noreply.github.com>
2025-09-24 18:43:36 -0600

    mend
    add pause action and keybindings

--- NoNameGame/Assets/Scripts/GameManager.cs
+++ NoNameGame/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,6 +9,8 @@ public class GameManager : MonoBehaviour
 
     static public GameObject player;
     static public GameManager Instance;
+    static public GameObject pauseMenu;
+    static public bool isPaused = false;
     void Awake()
     {
         _instance = this;
@@ -18,5 +21,21 @@ public class GameManager : MonoBehaviour
         Player p = player.GetComponent<Player>();
         p.TakeDamage(ammout);
     }
-    
+
+    /// <summary>
+    /// Toggles weather tha game is paused or not
+    /// </summary>
+    public void TogglePause()
+    {
+        isPaused = !pauseMenu.activeSelf;
+        pauseMenu.SetActive(isPaused);
+        Time.timeScale = isPaused ? 0f:1f;
+        //if (isPaused)
+        //{
+        //    EventSystem.current.SetSelectedGameObject(null);
+        //    EventSystem.current.SetSelectedGameObject(resumeBtn);
+        //}
+
+    }
+
 }

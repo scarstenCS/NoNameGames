7c97ecb8e00caa3bb696ca994fa3b50a5f4b8308
scarstenCS <scarsten@ualberta.ca>
2025-10-06 11:53:52 -0600

    comments

--- NoNameGame/Assets/Scripts/UpgradeManager.cs
+++ NoNameGame/Assets/Scripts/UpgradeManager.cs
@@ -12,27 +12,48 @@ public class UpgradeManager : MonoBehaviour
         _instance = this;
     }
 
+    /// <summary>
+    /// increased player's max health
+    /// </summary>
+    /// <param name="ammout">ammount to increase by</param>
     public void IncreaseMaxHealth(int ammout)
     {
         player.maxHealth += ammout;
         Debug.Log($"Max health increased by {ammout}");
     }
+    /// <summary>
+    /// Increases basic weapon damage 
+    /// </summary>
+    /// <param name="ammount">ammount to increase by</param>
     public void IncreaseWeaponDamage(int ammount)
     {
         player.basicWeaponDmg += ammount;
         Debug.Log($"Basic weapon damage increased by {ammount}");
     }
+
+    /// <summary>
+    /// Increases basic weapon distance
+    /// </summary>
+    /// <param name="ammount">ammount to increase by</param>
     public void IncreaseWeaponDistance(float ammount)
     {
         player.basicWeaponDistance += ammount;
         Debug.Log($"Basic weapon distance increased by {ammount}");
     }
+    /// <summary>
+    /// increases weapon speed
+    /// </summary>
+    /// <param name="ammount">Ammount to increase by</param>
     public void IncreaseWeaponSpeed(float ammount)
     {
         player.basicWeaponSpeed += ammount;
         Debug.Log($"Basic weapon speed increased by {ammount}");
     }
 
+    /// <summary>
+    /// Increases Player speed
+    /// </summary>
+    /// <param name="ammount">ammount to increase by</param>
     public void IncreasePlayerSpeed(float ammount)
     {
         player.Speed += ammount;

68ad67b37085f3f39015df0660b0fbf6f615dfe8
scarstenCS <scarsten@ualberta.ca>
2025-10-06 11:17:35 -0600

    add increase max health function

---
+++ NoNameGame/Assets/Scripts/UpgradeManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeManager : MonoBehaviour
+{
+    public Player player;
+    static public UpgradeManager Instance;
+    static private UpgradeManager _instance;
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public void IncreaseMaxHealth(int ammout)
+    {
+        player.maxHealth += ammout;
+    }
+    
+}

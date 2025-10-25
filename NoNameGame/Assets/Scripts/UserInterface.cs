f80719c6f568b69337ed66c5a5752aec7647fe17
Seanpiatt <112649852+Seanpiatt@users.noreply.github.com>
2025-10-25 11:54:46 -0600

    Updated Userinterface
    Created a ui bar to reduce the health bar based on the current health of the player.

--- NoNameGame/Assets/Scripts/UserInterface.cs
+++ NoNameGame/Assets/Scripts/UserInterface.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-
+using UnityEngine.UI;
 
 public class UserInterface : MonoBehaviour
 {
@@ -10,6 +10,9 @@ public class UserInterface : MonoBehaviour
     public Player player;
     public TMP_Text healthText;              // drag your "Health: ..." TMP Text
     public TMP_Text enemiesLeftText;         // drag your "Enemies left: ..." TMP Text
+    public GameObject damagedTakenImage;            
+    private RectTransform damagedTakenRectTransform; // cache RectTransform
+
     // Start is called before the first frame update
     void OnEnable()
     {
@@ -26,6 +29,10 @@ public class UserInterface : MonoBehaviour
     void Start()
     {
         Debug.Log($"[DEBUG] Initializing UI");
+        if (damagedTakenImage)
+        {
+            damagedTakenRectTransform = damagedTakenImage.GetComponent<RectTransform>();
+        }
         if (player != null) OnHealthChanged(player.Health, player.MaxHealth);
         // Debug.Log($"[DEBUG] Player health: {player.Health} / {player.maxHealth}"); // Debug line
         if (enemiesLeftText != null) enemiesLeftText.text = "Enemies left: 0";
@@ -34,8 +41,18 @@ public class UserInterface : MonoBehaviour
 
     private void OnHealthChanged(int current, int max)
     {
-        
+
         if (!healthText) return;
+        //currently the damage taken makes the rect grow, but not only to the right, i want to
+        // make it grow only to the left side
+        //now the height is set to 0 but should always be set to 8 pixels high
+        //Still not going to the left side correctly
+        if (damagedTakenRectTransform)
+        {
+            float damagePercent = 1f - ((float)current / (float)max);
+            float maxWidth = 64f; // adjust based on your image size
+            damagedTakenRectTransform.sizeDelta = new Vector2(damagePercent * maxWidth, 8f);
+        }
 
         healthText.text = $"Health: {current} / {max}";
     }
